2c72b2e6415246abe77ea6e7bb2c531fcf65e3b0
kardileshekhar07 <kardileshekhar07@gmail.com>
2026-07-30 00:35:41 -0400

    Updated homepage and dependencies

--- src/Pages/HomePage/index.js
+++ src/Pages/HomePage/index.js
@@ -2,6 +2,11 @@ import Header from "../../Componets/Header";
 // import "./HomePage.module.scss";
 import style from "./HomePage.module.scss";
 import Footer from "../../Componets/Footer";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../../../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../../../node_modules/bootstrap/dist/js/bootstrap.js";
+import "../../../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
 const Homepage = () => {
   const hName = "HEADER";
   const Pages = ["home", "About", "Profile", "Product", "Blog", "Contact"];
@@ -11,6 +16,7 @@ const Homepage = () => {
       <Header name={student} />
       <div className={style.HomePage}>
         <h3> THIS IS THE BODY</h3>
+        <h3 className="bg-primary"> Bootstrap stating </h3>
       </div>
       <Footer />
     </div>
--- src/index.js
+++ src/index.js
@@ -5,6 +5,11 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Homepage from "./Pages/HomePage";
 
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "../node_modules/bootstrap/dist/js/bootstrap.js";
+import "../node_modules/bootstrap/dist/js/bootstrap.min.js";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
